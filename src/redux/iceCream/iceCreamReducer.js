23d0bb425ce817d97a9d315d0d8229027c50778b
Elbie-em <emoonga1993@gmail.com>
2020-10-20 18:30:25 +0200

    Add IceCream Reducer

---
+++ src/redux/iceCream/iceCreamReducer.js
@@ -0,0 +1,21 @@
+const { BUY_ICECREAM } = require("./iceCreamTypes");
+
+const initialState = {
+  numOfIceCreams: 20
+}
+
+const iceCreamReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+         if(state.numOfIceCreams > 0 ) {
+           return {
+             ...state, numOfIceCreams: state.numOfIceCreams - 1
+           }
+         }else{
+           alert('Out of Stock')
+         }
+    default:
+     return state
+  }
+
+}
