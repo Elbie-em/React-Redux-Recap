696c556234d6ab93160b9c269c7e4794a97996d7
Elbie-em <emoonga1993@gmail.com>
2020-10-20 18:52:37 +0200

    Create root reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,12 @@
 import { combineReducers } from 'redux';
 
-import cakeReducer from './cake/cakeReducer';
+import cakeReducer from './cakes/cakeReducer';
 import iceCreamReducer from './iceCream/iceCreamReducer'
 
+
 const rootReducer = combineReducers({
-  cake:cakeReducer,
-  iceCream: iceCreamReducer,
-})
+  cake: cakeReducer,
+  iceCream : iceCreamReducer
+});
 
 export default rootReducer
