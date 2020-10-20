cb1852171d1942f24b0383a20580714b84ddef5a
Elbie-em <emoonga1993@gmail.com>
2020-10-21 01:22:59 +0200

    Implement ItemContainer component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './App.css';
 import { Provider } from 'react-redux';
 import store from './redux/store'
-import HooksCakeContainer from './components/HooksCakeContainer';
-import IceCreamContainer from './components/IceCreamContainer'
+import ItemContainer from './components/ItemContainer'
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <HooksCakeContainer />
-        <IceCreamContainer />
+        <ItemContainer cake />
+        <ItemContainer />
       </div>
     </Provider>
   );
