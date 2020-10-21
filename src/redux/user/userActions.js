080a94f849ac184d465af3df1984cdafcf16abf3
Elbie-em <emoonga1993@gmail.com>
2020-10-21 15:18:23 +0200

    Add fetchUser special function to call API endpoint for data

--- src/redux/user/userActions.js
+++ src/redux/user/userActions.js
@@ -1,21 +1,36 @@
 import {FETCH_USERS_REQUEST ,FETCH_USERS_SUCCESS, FETCH_USERS_FAILURE } from './userTypes'
-
-export const fetchUsersRequest = () => {
+import axios from 'axios'
+const fetchUsersRequest = () => {
   return {
     type: FETCH_USERS_REQUEST
   }
 }
 
-export const fetchUsersSuccess = (user) => {
+const fetchUsersSuccess = (user) => {
   return {
     type: FETCH_USERS_SUCCESS,
     payload: users
   }
 }
 
-export const fetchUsersFailure = (error) => {
+const fetchUsersFailure = (error) => {
   return {
     type: FETCH_USERS_FAILURE,
     payload: error
+  }
+}
+
+export const fetchUsers = () => {
+  return (dispatch) => {
+    dispatch(fetchUsersRequest);
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      const users = repose.data
+      dispatch(fetchUsersSuccess(users));
+    })
+    .catch(e => {
+      const errorMsg = e.message
+      dispatch(fetchUsersFailure(errorMsg));
+    })
   }
 }
